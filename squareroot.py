943001826dfbca8a753dbc8a701447ac9301f4d9
gouri <smchikkeri5757@gmail.com>
2025-10-18 12:19:58 +0530

    modified program to calcuate cube

--- squareroot.py
+++ squareroot.py
@@ -9,3 +9,14 @@ def calculate_square():
         print("Invalid input. Please enter a valid number.")
 if __name__ == "__main__":
     calculate_square()
+def calculate_cube():
+    try:
+        number_str = input("enter a number:")
+        number = float(number_str)
+        cube = number ** 3
+        print(f"The cube of {number} is {cube}")
+        breakpoint()
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+if __name__ == "__main__":
+    calculate_cube()
